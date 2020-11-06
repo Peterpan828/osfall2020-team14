f69bcabeffa772032e7f06622cadc18a0c834197
SNU_Peterpan <alswo901@snu.ac.kr>
2020-11-06 14:23:33 +0900

    Modified Test file

--- test/trial.c
+++ test/trial.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sched.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -7,8 +10,12 @@
 #define SCHED_SETWEIGHT 398
 #define SCHED_GETWEIGHT 399
 
-void trial_division(int num)
+void trial_division(int num, pid_t pid)
 {	
+	int weight;
+	weight = syscall(SCHED_GETWEIGHT, getpid());
+	printf("PID : %d  Weight : %d\n", getpid(), weight);
+
 	int f = 2;
 	while(num > 1)
 	{
@@ -24,20 +31,26 @@ void trial_division(int num)
 
 int main(int argc, char *argv[])
 {
-	//assert(argc == 3);
-	//int procs = argv[1];
-	//int weight = argv[2];
-
+	assert(argc == 2);
+	int procs = atoi(argv[1]);
 	int target = 1837158; // random number?
+	pid_t pid;
 	
-	printf("Current Process's Scheduler is %d\n", sched_getscheduler(0));
+	//printf("Current Process's Scheduler is %d\n", sched_getscheduler(0));
 	
 	struct sched_param wrr_param;
 	wrr_param.sched_priority = 0;
 	
-	printf("Return Value of syscall is %d\n", sched_setscheduler(0,7,&wrr_param));
-	//perror("");
+	sched_setscheduler(0,7,&wrr_param);
 	
-	printf("Current Process's Scheduler is %d\n", sched_getscheduler(0));
-	trial_division(target);
+	//printf("Current Process's Scheduler is %d\n", sched_getscheduler(0));
+
+	for(int i=0;i<procs;i++)
+	{
+		pid = fork();
+		if(pid < 0)	printf("Error\n");
+	}
+
+	trial_division(target, getpid());
+	return 0;
 }
